5eda5e588ff81c7355ae38a465b70ed9af861004
Sutormin Evgeniy <Sutormin@competentum.ru>
2017-10-18 17:35:55 +0300

    start project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>React</h1>
+    </div>
+  );
+}
+
+ReactDom.render(<App />, document.getElementById('root'));
