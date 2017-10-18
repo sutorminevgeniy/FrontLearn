bd0bbf8592e989334ba1f9695b6831dfcad5f8f1
sutorminevgeniy <sutormin.evgeniy@yandex.ru>
2017-10-19 01:11:41 +0300

    webpack build

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import './style.scss'
+
 function App() {
   return (
     <div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,35 @@
-module.exports = {};
+module.exports = {
+  entry: './src/App.jsx',
+
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/public'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$|\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader'
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']  // for loading ".jsx" files
+  },
+
+  devServer: {
+    contentBase: './public',
+    port: 3030
+  },
+
+  devtool: 'eval-source-map'  // for debugging
+};
+
+// !!! разобраться с подключением react-hot-loader
