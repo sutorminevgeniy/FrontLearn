373b3624f4ad7dfae6cc9bdad792223c70c09fd9
Sutormin Evgeniy <Sutormin@competentum.ru>
2017-10-31 19:12:57 +0300

    add ChangeLevel

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,17 @@ class App extends React.Component {
 
     this.state = {
       currentLevel: 0,
-      currentLang: 'ru'
+      currentLang: 'ru',
+      levelsShow: false
     };
 
     this.nextLevel = this.nextLevel.bind(this);
     this.prevLevel = this.prevLevel.bind(this);
+    this.changeLevel = this.changeLevel.bind(this);
+    this.toggleLevels = this.toggleLevels.bind(this);  
   }
 
+  // Следующий уровень
   nextLevel() {
     let nextLevel = this.state.currentLevel;
 
@@ -28,6 +32,7 @@ class App extends React.Component {
     }
   }
 
+  // Предыдущий уровень
   prevLevel() {
     let prevLevel = this.state.currentLevel;
 
@@ -37,9 +42,33 @@ class App extends React.Component {
     }
   }
 
+  // Выбор уровня
+  changeLevel(e) {
+    let level = parseInt(e.target.dataset.level);
+
+    this.setState({ currentLevel: level });
+  }
+
+  // Открытие закрытие окна с уровнями
+  toggleLevels() {
+    this.setState({ levelsShow: !this.state.levelsShow });
+  }
+
+  // Проверка перед обнавлением состояния или свойств
+  shouldComponentUpdate(nextProps, nextState) {
+    if(this.state.levelsShow) {
+      nextState.levelsShow = false;
+    }
+
+    return true;
+  }
+
   render() {
     let dataLevel = levels[this.state.currentLevel];
     let lang = this.state.currentLang;
+    const styleShow = {
+      display: "block"
+    };
 
     return (
       <div>
@@ -47,7 +76,7 @@ class App extends React.Component {
           <div>
             <div id="level-counter">
               <span className="arrow left" onClick={ this.prevLevel }>◀</span>
-              <span id="level-indicator">
+              <span id="level-indicator" onClick={ this.toggleLevels }>
                 <span id="labelLevel" className="translate">{ messages.labelLevel[lang] } </span>
                 <span className="current">{ this.state.currentLevel + 1 }</span>
                 <span id="labelOf" className="translate"> { messages.labelOf[lang] } </span>
@@ -55,15 +84,29 @@ class App extends React.Component {
                 <span className="caret">▾</span>
               </span>
               <span className="arrow right" onClick={ this.nextLevel }>▶</span>
-              <div id="levelsWrapper" className="tooltip">
-                <div id="levels"></div>
-                <div id="labelReset" className="translate">{ messages.labelReset[lang] }</div>
+              <div id="levelsWrapper" className="tooltip" style={ this.state.levelsShow ? styleShow : {} }>
+                <div id="levels">
+                  {
+                    levels.map((item, i) => {
+                      return (
+                        <span 
+                          key={ i } 
+                          className={"level-marker" + ( i === this.state.currentLevel ? " solved current" : "")}
+                          onClick={ this.changeLevel }
+                          data-level={ i } >{ i + 1 }</span>
+                      );
+                    })
+                  }
+                </div>
+                <div id="labelReset" className="translate" onClick={ this.toggleLevels }>{ messages.labelReset[lang] }</div>
               </div>
             </div>
+
             <h1>Flexbox Froggy</h1>
             <p id="instructions" dangerouslySetInnerHTML={{__html: dataLevel.instructions[lang]}}></p>
             <p id="docs"></p>
           </div>
+
           <div id="editor">
             <div id="css">
               <div className="line-numbers">1<br/>2<br/>3<br/>4<br/>5<br/>6<br/>7<br/>8<br/>9<br/>10</div>
@@ -73,6 +116,7 @@ class App extends React.Component {
             </div>
             <button id="next" className="translate">{ messages.next[lang] }</button>
           </div>
+
           <div id="share">
             <p className="img-next">
               <a href="http://cssgridgarden.com"><img src="./images/next-gridgarden.png"/></a>
@@ -86,7 +130,6 @@ class App extends React.Component {
               <span className="fb-like" data-href="http://flexboxfroggy.com" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></span>
             </p>
           </div>
-
           <div className="credits">
             <span id="labelFooter" className="translate">{ messages.labelFooter[lang] } </span>
             <a href="https://github.com/thomaspark/flexboxfroggy/">GitHub</a> •
@@ -105,6 +148,7 @@ class App extends React.Component {
             <span id="gridGarden" className="translate">Want to learn CSS grid? Play</span> <a href="http://cssgridgarden.com">Grid Garden</a>.
           </div>
         </section>
+
         <section id="view">
           <div id="board">
             <div id="pond">
