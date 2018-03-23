6d6e1404a244eb0e5daed033cd9fe403bb22da72
Sutormin Evgeniy <Sutormin@competentum.ru>
2018-03-23 16:43:06 +0300

    login

--- admin/src/reducers/user.js
+++ admin/src/reducers/user.js
@@ -3,7 +3,7 @@ import { GET_USER, EXIT_USER } from '../actions';
 function reducer(state = null, action) {
   switch (action.type) {
     case GET_USER:
-      return (action.user.role === 'admin' ? action.user : null);
+      return (action.user && action.user.role === 'admin' ? action.user : null);
 
     case EXIT_USER:
       return null;
